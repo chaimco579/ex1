f7062e6d6946a26b4d07f1edc4b02c6aa1e4495e
chaimco579 <160393280+chaimco579@users.noreply.github.com>
2024-02-28 14:20:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def helloworld():
     return 'Hello, Feature1!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
     
